5232f85fcd4fad4bc0fc604e15bb131bd2e6609f
Nitrox14 <sahayamalamathi@gmail.com>
2021-12-07 23:37:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,16 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setz(30);
 
 renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10,3,16,100) 
+const material = new THREE.MeshBasicMaterial({color:0xFF6347, wireframe:true});
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus)
+
+function animate(){
+    requestAnimationFrame (animate);
+    renderer.render(scene, camera);
+}
+
+animate()
